1591c40f235437cd2ac2d016eb55f31c163cd005
aerosonicdb <135924499+aerosonicdb@users.noreply.github.com>
2023-06-29 13:46:02 +0930

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='aerosonicdb',
-    version='0.2.2',
+    version='0.3.0',
     description='Download and get started with the AeroSonicDB-YPAD0523 audio dataset of low-flying aircraft.',
     author='Blake Downward',
     author_email='aerosonicdb@gmail.com',
     url='https://github.com/aerosonicdb/AeroSonicDB-YPAD0523',
     packages=find_packages(),
-    install_requires=['pandas', 'jupyter', 'notebook']
+    install_requires=['librosa', 'pandas', 'jupyter', 'notebook']
 )
