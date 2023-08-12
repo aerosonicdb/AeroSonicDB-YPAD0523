982c2b1bdba6e1008179946d8d773746804c06b4
aerosonicdb <135924499+aerosonicdb@users.noreply.github.com>
2023-08-12 09:50:19 +0930

    Add files via upload

--- aerosonicdb/models/mfcc/cnn.py
+++ aerosonicdb/models/mfcc/cnn.py
@@ -1,13 +1,13 @@
-import json
 import os
 import numpy as np
 import tensorflow.keras as keras
 import tensorflow as tf
-from sklearn.model_selection import StratifiedGroupKFold
-from sklearn.model_selection import cross_validate
 from scikeras.wrappers import KerasClassifier
-from sklearn.model_selection import GroupShuffleSplit
+from sklearn.model_selection import cross_validate
 from aerosonicdb.utils import get_project_root
+from aerosonicdb.utils import fetch_k_fold_cv_indicies
+from aerosonicdb.utils import load_train_data
+from aerosonicdb.utils import train_val_split
 import absl.logging
 absl.logging.set_verbosity(absl.logging.ERROR)
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.FATAL)
@@ -16,44 +16,6 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.FATAL)
 root_path = get_project_root()
 train_path = os.path.join(root_path, 'data/processed/13_mfcc_5_train.json')
 output_path = os.path.join(root_path, 'models')
-
-
-def load_data(data_path, target_labels):
-    with open(data_path, "r") as fp:
-        data = json.load(fp)
-
-    # convert lists to numpy arrays
-    X = np.array(data["mfcc"])
-    y = np.array(data[target_labels]).astype(int)
-    g = np.array(data['fold_label'])
-
-    print("Data successfully loaded!")
-
-    return X, y, g
-
-
-# function to return the cv splits by index
-def fetch_cv_indicies(x, y, g):
-    sgkf = StratifiedGroupKFold(n_splits=10)
-    sgkf.get_n_splits(x, y)
-    cv_splits = sgkf.split(x, y, g)
-
-    return cv_splits
-
-
-def train_val_split(x, y, g, val_size=0.1, rand_seed=0):
-    gs = GroupShuffleSplit(n_splits=2, test_size=val_size, random_state=rand_seed)
-    train_index, val_index = next(gs.split(x, y, groups=g))
-
-    x_train, x_val = x[train_index], x[val_index]
-    y_train, y_val = y[train_index], y[val_index]
-
-    print(f'Length of X train: {len(x_train)}')
-    print(f'Length of y train: {len(y_train)}')
-    print(f'Length of X val: {len(x_val)}')
-    print(f'Length of y val: {len(y_val)}')
-
-    return x_train, y_train, x_val, y_val
 
 
 def build_model(input_shape):
@@ -96,30 +58,37 @@ def init_model(x):
                   loss='BinaryCrossentropy',
                   metrics=['binary_accuracy'])
 
-    model.summary()
+    # model.summary()
 
     return model
 
 
-def run_cv(rand_seed=0):
-    X, y, g = load_data(data_path=train_path, target_labels='class_label')
+def run_cv(train_path=train_path, epochs=1, batch_size=216, rand_seed=0, verbose=0, k=10):
+    keras.utils.set_random_seed(rand_seed)
+    X, y, g = load_train_data(data_path=train_path, target_labels='class_label')
     build = init_model(X)
-    model = KerasClassifier(model=build, epochs=50, batch_size=216, random_state=rand_seed, verbose=2)
+    model = KerasClassifier(model=build, epochs=epochs, batch_size=batch_size, random_state=rand_seed, verbose=verbose)
 
-    results = cross_validate(model, X, y, cv=fetch_cv_indicies(X, y, g))
+    results = cross_validate(model, X, y, cv=fetch_k_fold_cv_indicies(X, y, g, k=k))
     mean = results['test_score'].mean() * 100
     st_dev = results['test_score'].std() * 100
 
-    print(f"Acc: %.2f%% (%.2f%%)" % (mean, st_dev))
+    print(f'Acc: %.2f%% (%.2f%%)' % (mean, st_dev))
 
     return mean, st_dev
 
 
-def train_save_model(dir_path=output_path,
-                     mfcc_path=train_path,
-                     filename='mfcc_cnn'):
+def train_save_model(output_path=output_path,
+                     train_path=train_path,
+                     filename='mfcc_cnn', 
+                     epochs=1, 
+                     batch_size=216, 
+                     verbose=0,
+                     rand_seed=0):
+    
+    keras.utils.set_random_seed(rand_seed)
 
-    X, y, g = load_data(data_path=mfcc_path, target_labels='class_label')
+    X, y, g = load_train_data(data_path=train_path, target_labels='class_label')
 
     X_train, y_train, X_val, y_val = train_val_split(X, y, g)
 
@@ -127,12 +96,11 @@ def train_save_model(dir_path=output_path,
     model.summary()
 
     # train model
-    model.fit(X_train, y_train, validation_data=(X_val, y_val), batch_size=216, epochs=50)
+    model.fit(X_train, y_train, validation_data=(X_val, y_val), batch_size=batch_size, epochs=epochs, verbose=verbose)
 
     # save the model
-    model_path = os.path.join(dir_path, filename, 'model')
+    model_path = os.path.join(output_path, filename, 'model')
     model.save(model_path)
+    
+    print(f'Model saved to {model_path}.\n')
 
-
-if __name__ == "__main__":
-    train_save_model()
--- aerosonicdb/models/mfcc/logistic_regression.py
+++ aerosonicdb/models/mfcc/logistic_regression.py
@@ -2,10 +2,11 @@ import os
 import json
 import pickle
 import numpy as np
-from sklearn.model_selection import StratifiedGroupKFold
 from sklearn.model_selection import cross_validate
 from sklearn.linear_model import LogisticRegression
 from aerosonicdb.utils import get_project_root
+from aerosonicdb.utils import fetch_k_fold_cv_indicies
+from aerosonicdb.utils import load_frame_train_data
 
 root_path = get_project_root()
 train_path = os.path.join(root_path, 'data/processed/13_mfcc_5_train.json')
@@ -15,72 +16,33 @@ if not os.path.exists(output_path):
     os.mkdir(output_path)
 
 
-def explode_3_dims(x, y, g):
-    x_expand = []
-    y_expand = []
-    g_expand = []
-
-    for i in range(x.shape[0]):
-        for n in range(x.shape[1]):
-            x_expand.append(x[i][n])
-            y_expand.append(y[i])
-            g_expand.append(g[i])
-
-    x_expand = np.array(x_expand)
-    y_expand = np.array(y_expand)
-    g_expand = np.array(g_expand)
-
-    return x_expand, y_expand, g_expand
-
-
-def load_train_data(data_path, target_label):
-    with open(data_path, "r") as fp:
-        data = json.load(fp)
-
-    # convert lists to numpy arrays
-    X = np.array(data["mfcc"])
-    y = np.array(data[target_label]).astype(int)
-    g = np.array(data['fold_label'])
-
-    print("Train data successfully loaded!")
-
-    X, y, g = explode_3_dims(x=X, y=y, g=g)
-
-    return X, y, g
-
-
-# function to return the cv splits by index
-def fetch_cv_indicies(x, y, g):
-    sgkf = StratifiedGroupKFold(n_splits=10)
-    sgkf.get_n_splits(x, y)
-    cv_splits = sgkf.split(x, y, g)
-
-    return cv_splits
-
-
-def run_cv(rand_seed=0):
-    X, y, g = load_train_data(data_path=train_path, target_label='class_label')
+def run_cv(train_path=train_path, k=10, rand_seed=0):
+    X, y, g = load_frame_train_data(data_path=train_path, target_label='class_label')
 
     model = LogisticRegression(random_state=rand_seed, max_iter=400)
 
-    results = cross_validate(model, X, y, cv=fetch_cv_indicies(X, y, g), n_jobs=-1)
+    results = cross_validate(model, X, y, cv=fetch_k_fold_cv_indicies(X, y, g, k=k), n_jobs=-1)
     mean = results['test_score'].mean() * 100
     st_dev = results['test_score'].std() * 100
 
-    print(f"Acc: %.2f%% (%.2f%%)" % (mean, st_dev))
+    print(f'Acc: %.2f%% (%.2f%%)' % (mean, st_dev))
 
     return mean, st_dev
 
 
-def save_model(filename='mfcc_logistic_regression.sav', rand_seed=0):
-    X, y, g = load_train_data(data_path=train_path, target_label='class_label')
+def train_save_model(train_path=train_path, output_path=output_path, filename='mfcc_logistic_regression.sav', rand_seed=0):
+    
+    X, y, g = load_frame_train_data(data_path=train_path, target_label='class_label')
 
     model = LogisticRegression(random_state=rand_seed, max_iter=400)
+    
     # Fit the model on training set
-
     model.fit(X, y)
-    # save the model to disk
+    
+    # save the model
     filename = os.path.join(output_path, filename)
     pickle.dump(model, open(filename, 'wb'))
+    
+    print(f'Model saved to: {filename}')
 
 
