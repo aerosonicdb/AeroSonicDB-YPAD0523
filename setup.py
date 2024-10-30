4e6dcdfeeab2a816ce2779d5c0faf0e2cfcf67c7
Blake Downward <54337911+blakedownward@users.noreply.github.com>
2024-10-30 16:32:09 +1030

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aerosonicdb',
-    version='1.0.0',
+    version='1.0.1',
     description='Download and get started with the AeroSonicDB-YPAD0523 audio dataset of low-flying aircraft.',
     author='Blake Downward',
     author_email='aerosonicdb@gmail.com',
